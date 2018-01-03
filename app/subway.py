43785876bac13cb00d5180262c1a64de0e149cba
gfertel <greg.fertel@adroll.com>
2018-01-03 17:23:36 -0500

    big update

--- app/subway.py
+++ app/subway.py
@@ -1,11 +1,20 @@
-import urllib2, datetime
+import urllib2, datetime, requests, json
 import nyct_subway_pb2
 from google.transit import gtfs_realtime_pb2 
 
 API_KEY = "a27db90d779ca74d641c367e47ddad2a"
 FEED_ID = 2
+CLIENT_ID = '65a0b73ea161e22c90b89413b91c0750'
+COLORS = {
+    'A': '#0039A6', 'C': '#0039A6', 'E': '#0039A6',
+    'B': '#FF6319', 'D': '#FF6319', 'F': '#FF6319', 'M': '#FF6319',
+    'G': '#6CBE45', 'J': '#996633', 'Z': '#996633', 'L': '#A7A9AC',
+    'N': '#FCCC0A', 'Q': '#FCCC0A', 'R': '#FCCC0A', 'S': '#808183',
+    '1': '#EE352E', '2': '#EE352E', '3': '#EE352E', 
+    '4': '#00933C', '5': '#00933C', '6': '#00933C', '7': '#B933AD'
+}
 
-def get_realtime_subway(train, feed_id = FEED_ID, key = API_KEY):
+def get_realtime_subway(train, feed_id=FEED_ID, key=API_KEY):
     feed = gtfs_realtime_pb2.FeedMessage() 
     url = 'http://datamine.mta.info/mta_esi.php?key=%s&feed_id=%s' % (key, feed_id)
     response = urllib2.urlopen(url)
@@ -16,3 +25,13 @@ def get_realtime_subway(train, feed_id = FEED_ID, key = API_KEY):
             if stop_time_update.stop_id == train:
                 arrival_times.append(datetime.datetime.fromtimestamp(stop_time_update.arrival.time))
     return sorted(arrival_times)
+
+
+def get_realtime_subway_elixir(station_id, client_id=CLIENT_ID):
+    url = "http://app-ex-mta.herokuapp.com/times/%s?client_id=%s" % (station_id, client_id)
+    response = requests.get(url)
+    data = response.json()
+    print data
+    return json.dumps(data)
+
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import json, os, sys, datetime, logging, time
 import flask
 from app import app
+import forms
 import weather, subway, citibike
 import requests
 
@@ -10,20 +11,37 @@ app.logger.setLevel(logging.ERROR)
 ADDRESS = "303 Graham Avenue, Brooklyn, NY 11211"
 LATITUDE = 40.7131728
 LONGITUDE = -73.94442019999997
+with open(os.getcwd() + '/app/static/all_stations.json') as f:
+    STATION_DICTS = json.loads(f.read())['result']
+STATIONS = {x['name']: x['id'][:-1] for x in STATION_DICTS}
+
+@app.route('/_autocomplete', methods=['GET'])
+def autocomplete():
+    return flask.Response(json.dumps(STATIONS.keys()), mimetype='application/json')
+
+
+#@app.route('/start', methods=['GET', 'POST'])
+#def start():
+#    if flask.request.args.get('address') and flask.request.args.get('subway'):
+#        return "WOO"
+#    else:
+#        form = forms.MainForm(flask.request.form)
+#        return flask.render_template("start.html", form=form)
+
 
 @app.route('/crypto')
 def crypto_api():
-	url = 'https://api.coinmarketcap.com/v1/ticker/'
-	resp = requests.get(url)
-	data = resp.json()
-	prices = {}
-	for x in data:
-		formatted_price = '${:,.2f}'.format(float(x['price_usd']))
-		percent_change = x['percent_change_24h']
-		prices[x['name']] = {'price': formatted_price, 'change': percent_change}
-	return json.dumps(prices)
+    url = 'https://api.coinmarketcap.com/v1/ticker/'
+    resp = requests.get(url)
+    data = resp.json()
+    prices = {}
+    for x in data:
+        formatted_price = '${:,.2f}'.format(float(x['price_usd']))
+        percent_change = x['percent_change_24h']
+        prices[x['name']] = {'price': formatted_price, 'change': percent_change}
+    return json.dumps(prices)
 
-@app.route('/subway')
+"""@app.route('/subway')
 def subway_api():
     display_time = datetime.datetime.now()
     # Get Subway Info
@@ -38,10 +56,27 @@ def subway_api():
             except:
                 print "subway error: sleeping 5 seconds"
                 time.sleep(5)
-    return json.dumps(subways)
+    return json.dumps(subways)"""
+
+@app.route('/subway')
+def subway_api(station_id=None):
+    if not station_id:
+        station_id = flask.request.args.get('station_id')
+    if station_id:
+        return subway.get_realtime_subway_elixir(station_id)
+    else:
+        response = app.response_class(
+            response=json.dumps({'error': 'missing station_id parameter'}),
+            status=422,
+            mimetype='application/json'
+            )
+        return response
 
 @app.route('/citibike')
-def citibike_api(latitude=LATITUDE, longitude=LONGITUDE):
+def citibike_api(latitude=None, longitude=None):
+    if not latitude and not longitude:
+        latitude = float(flask.request.args.get('latitude'))
+        longitude = float(flask.request.args.get('longitude'))
     citibike_data = citibike.get_citibike_data()
     station_info = citibike.get_citibike_availability(citibike_data, latitude, longitude)
     return json.dumps(station_info)
@@ -51,19 +86,32 @@ def citibike_api(latitude=LATITUDE, longitude=LONGITUDE):
 @app.route('/')
 @app.route('/index')
 def page():
-    address = "303 Graham Avenue, Brooklyn, NY 11211"
-    #(latitude, longitude) = weather.get_latitude_longitude(address)
-    # latitude and longitude for 303 Graham Avenue, Brooklyn NY 11211
-    latitude = 40.7131728
-    longitude = -73.94442019999997
+    address = flask.request.args.get('address') or flask.request.cookies.get('address')
+    subway_choice = flask.request.args.get('subway') or flask.request.cookies.get('subway')
+    latitude = flask.request.cookies.get('latitude')
+    longitude = flask.request.cookies.get('longitude')
+    if not address and not subway_choice:
+        form = forms.MainForm(flask.request.form)
+        return flask.render_template("start.html", form=form)
+    elif not latitude and not longitude:
+        resp = flask.make_response(flask.redirect('/'))
+        resp.set_cookie('address', address)
+        resp.set_cookie('subway', flask.request.args.get('subway'))
+        (latitude, longitude) = weather.get_latitude_longitude(address)
+        resp.set_cookie('latitude', str(latitude))
+        resp.set_cookie('longitude', str(longitude))
+        return resp
+    directions = {}
+    directions[STATIONS[subway_choice] + 'N'] = 'Manhattan'
+    directions[STATIONS[subway_choice] + 'S'] = 'Rockaway Parkway'
 
     time_updated = datetime.datetime.now()
     # running off local computer, otherwise would need to convert from UTC to EST here
     display_time = time_updated - datetime.timedelta(hours=0)
-    directions = {"L11N":"Manhattan", "L11S":"Rockaway Parkway"}
+    #directions = {"L11N":"Manhattan", "L11S":"Rockaway Parkway"}
 
     
-    embed_src = weather.get_forecast_embed_url("#B08B71", latitude, longitude, "303 Graham Avenue, Brooklyn, NY")
+    embed_src = weather.get_forecast_embed_url("", latitude, longitude, address)
 
     # Update Plotly
     graph_intensity = False
@@ -72,9 +120,11 @@ def page():
     if max([minute['precipIntensity'] for minute in weather_data['minutely']]) > 0.005:
         graph_intensity = True"""
     # Get Citibike Info
-    subways = json.loads(subway_api())
-    station_info = json.loads(citibike_api())
+    subways = json.loads(subway_api(STATIONS[subway_choice]))
+    station_info = json.loads(citibike_api(latitude, longitude))
     return flask.render_template("site.html", embed_src = embed_src, 
         graph_intensity=graph_intensity, 
         time=datetime.datetime.strftime(display_time, "%I:%M:%S %p"), 
-        subways=subways, directions=directions, station_info=station_info)
+        subways=subways, subway_station_name=subway_choice, 
+        directions=directions, station_info=station_info, 
+        latitude=latitude, longitude=longitude, colors=subway.COLORS)
